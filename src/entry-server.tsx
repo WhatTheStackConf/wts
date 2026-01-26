6abbb912b5a0f25dc1cb95eb34a7e86931ab24c3
Darko Bozhinovski <dbozhinovski@gmail.com>
2026-01-26 20:11:09 +0100

    feat: adds umami

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -11,6 +11,11 @@ export default createHandler(() => (
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
           {assets}
+          <script
+            defer
+            src="https://umami.foundry.mk/script.js"
+            data-website-id="7eac874e-f8d2-4d48-8b71-aa34d1b2cd78"
+          ></script>
         </head>
         <body>
           <div id="app">{children}</div>
