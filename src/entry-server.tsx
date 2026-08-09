7066e16f72ebfa4a8d1b8b8e87ebc419d6f06922
Ilija Boshkov <ilija@codechem.com>
2026-08-09 17:21:43 +0200

    feat: add Meta Pixel tracking

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -16,8 +16,29 @@ export default createHandler(() => (
             src="https://umami.foundry.mk/script.js"
             data-website-id="7eac874e-f8d2-4d48-8b71-aa34d1b2cd78"
           ></script>
+          <script>
+            {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2475235909644118');
+fbq('track', 'PageView');`}
+          </script>
         </head>
         <body>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style="display:none"
+              src="https://www.facebook.com/tr?id=2475235909644118&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
           <div id="app">{children}</div>
           {scripts}
         </body>
